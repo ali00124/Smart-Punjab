de24cd14c4bfe6d5876e0624b490f8a764a5c8fa
Muhammad Ali <alim@dnbsame.com>
2026-08-29 14:52:42 +0500

    a

--- app.py
+++ app.py
@@ -27,9 +27,8 @@ tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9, tab10, tab11, tab12 = st.t
     "POL",
     "Audit",
     "Forecast Calendar",
-    "Promotions",
     "Officers",
-    "Book1"
+    "SLDR Promotions"
 ])
 
 with tab1:
